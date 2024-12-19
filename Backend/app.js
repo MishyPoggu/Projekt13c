7d0316b470533a513710987df430e21bde5cd171
MishyPoggu <karsai.mihaly.ferenc.2020@osztalyterem.katolikuskeri.hu>
2024-12-19 17:28:42 +0100

    Köszi szépen

---
+++ Backend/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+require("dotenv").config();
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
