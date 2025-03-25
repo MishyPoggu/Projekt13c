8c9ba732882247ec9de25c70537546d1d95b4d9e
KlubertAdam <klubert.adam.2020@osztalyterem.katolikuskeri.hu>
2025-03-25 20:45:14 +0100

    raktár stílus
    Félig kész

--- Frontend/project/src/app/raktar/raktar.component.ts
+++ Frontend/project/src/app/raktar/raktar.component.ts
@@ -5,7 +5,6 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { UserService } from '../services/user.service';
-import { UserMachine } from '../user-machine';
 
 @Component({
   selector: 'app-raktar',
@@ -20,12 +19,13 @@ export class RaktarComponent implements OnInit, AfterViewInit {
   arcades: Arcade[] = [];
   pinballMachines: Arcade[] = [];
   consoles: Arcade[] = [];
-
+  
   arcade: Arcade = { id: 0, name: "", genre: "", publisher: "", release: "" };
 
   searchTerm: string = '';
   publisherFilter: string = '';
   genreFilter: string = '';
+  releaseFilter: string = '';
 
   filteredArcades: Arcade[] = [];
   filteredPinballs: Arcade[] = [];
@@ -34,100 +34,76 @@ export class RaktarComponent implements OnInit, AfterViewInit {
   uniquePublishers: string[] = [];
   uniqueGenres: string[] = [];
 
-  constructor(private arcadeService: ArcadeService, private userService:UserService) { }
+  userMachines: any;
+  arcadeMachines: any;
+  consolesMachines: any;
+  pinball: any;
 
-  addMachineToUser(arcade:Arcade, machineType:string) {
-    this.userService.addMachineToUser(arcade, machineType).subscribe({
-      next:(res:any)=> {
-        window.location.reload();
-      },
-      error:(err:HttpErrorResponse)=> {
-        alert(err.message)
-      }
-    })
-  }
-
-  userMachines:any;
-  arcadeMachines:any;
-  consolesMachines:any;
-  pinball:any;
+  constructor(private arcadeService: ArcadeService, private userService: UserService) { }
 
   ngOnInit(): void {
-
-    this.userService.getUserMachines(Number(localStorage.getItem("userId"))).subscribe({
-      next:(res:any)=> {
-        this.userMachines = res.machines;
-        this.arcadeMachines=this.userMachines.arcadeMachines;
-        this.consolesMachines=this.userMachines.consoles;
-        this.pinball=this.userMachines.pinballMachines;
-      },
-      error:(err:HttpErrorResponse)=> {
-        alert(err.message)
-      }
-    })
-
-
-
-
-
-    this.arcadeService.getAllArcade().subscribe({
-      next: (res: any) => {
-        this.arcades = res.data;
-        this.filteredArcades = res.data; 
-        this.uniquePublishers = this.getUniquePublishers(this.arcades);
-        this.uniqueGenres = this.getUniqueGenres(this.arcades);
-      },
-      error: (err: HttpErrorResponse) => {
-        alert(err.message);
-      }
-    });
-
-    this.arcadeService.getAllPinballMachines().subscribe({
-      next: (res: any) => {
-        this.pinballMachines = res.data;
-        this.filteredPinballs = res.data;
-        this.uniquePublishers = [...new Set([...this.uniquePublishers, ...this.getUniquePublishers(this.pinballMachines)])];
-        this.uniqueGenres = [...new Set([...this.uniqueGenres, ...this.getUniqueGenres(this.pinballMachines)])];
-      },
-      error: (err: HttpErrorResponse) => {
-        alert(err.message);
-      }
-    });
-
-    this.arcadeService.getAllConsole().subscribe({
-      next: (res: any) => {
-        this.consoles = res.data;
-        this.filteredConsoles = res.data;
-        this.uniquePublishers = [...new Set([...this.uniquePublishers, ...this.getUniquePublishers(this.consoles)])];
-        this.uniqueGenres = [...new Set([...this.uniqueGenres, ...this.getUniqueGenres(this.consoles)])];
-      },
-      error: (err: HttpErrorResponse) => {
-        alert(err.message);
-      }
-    });
+    this.loadUserMachines();
+    this.loadArcades();
+    this.loadPinballs();
+    this.loadConsoles();
   }
 
   ngAfterViewInit(): void {
+    this.initializeVideoPlayer();
+  }
+
+  loadUserMachines() {
+    this.userService.getUserMachines(Number(localStorage.getItem("userId"))).subscribe({
+      next: (res: any) => {
+        this.userMachines = res.machines;
+        this.arcadeMachines = this.userMachines.arcadeMachines;
+        this.consolesMachines = this.userMachines.consoles;
+        this.pinball = this.userMachines.pinballMachines;
+      },
+      error: (err: HttpErrorResponse) => alert(err.message)
+    });
+  }
+
+  loadArcades() {
+    this.arcadeService.getAllArcade().subscribe({
+      next: (res: any) => {
+        this.arcades = res.data;
+        this.filteredArcades = [...this.arcades];
+        this.updateUniqueFilters();
+      },
+      error: (err: HttpErrorResponse) => alert(err.message)
+    });
+  }
+
+  loadPinballs() {
+    this.arcadeService.getAllPinballMachines().subscribe({
+      next: (res: any) => {
+        this.pinballMachines = res.data;
+        this.filteredPinballs = [...this.pinballMachines];
+        this.updateUniqueFilters();
+      },
+      error: (err: HttpErrorResponse) => alert(err.message)
+    });
+  }
+
+  loadConsoles() {
+    this.arcadeService.getAllConsole().subscribe({
+      next: (res: any) => {
+        this.consoles = res.data;
+        this.filteredConsoles = [...this.consoles];
+        this.updateUniqueFilters();
+      },
+      error: (err: HttpErrorResponse) => alert(err.message)
+    });
+  }
+
+  initializeVideoPlayer(): void {
     const video = this.videoPlayer.nativeElement;
     video.load();
-
-    const tryPlay = () => {
-      video.play().catch((error) => {
-        console.warn('Videó nem tudott elindulni automatikusan:', error);
-      });
-    };
-
-    tryPlay();
-
-    if (video.paused) {
-      const playOnInteraction = () => {
-        tryPlay();
-        document.removeEventListener('click', playOnInteraction);
-        document.removeEventListener('touchstart', playOnInteraction);
-      };
-      document.addEventListener('click', playOnInteraction);
-      document.addEventListener('touchstart', playOnInteraction);
-    }
+    video.play().catch(() => {
+      document.addEventListener('click', () => video.play(), { once: true });
+      document.addEventListener('touchstart', () => video.play(), { once: true });
+    });
   }
 
   openData(arcade: Arcade) {
@@ -136,36 +112,37 @@ export class RaktarComponent implements OnInit, AfterViewInit {
 
   filterData() {
     const lowerCaseSearchTerm = this.searchTerm.toLowerCase();
-  
-    this.filteredArcades = this.arcades.filter((arcade) =>
-      (arcade.name.toLowerCase().includes(lowerCaseSearchTerm) ||
-        arcade.genre.toLowerCase().includes(lowerCaseSearchTerm) ||
-        arcade.publisher.toLowerCase().includes(lowerCaseSearchTerm)) &&
-      (this.publisherFilter ? arcade.publisher.toLowerCase().includes(this.publisherFilter.toLowerCase()) : true) &&
-      (this.genreFilter ? arcade.genre.toLowerCase().includes(this.genreFilter.toLowerCase()) : true)
-    );
-  
-    this.filteredPinballs = this.pinballMachines.filter((pinball) =>
-      (pinball.name.toLowerCase().includes(lowerCaseSearchTerm) ||
-        pinball.genre.toLowerCase().includes(lowerCaseSearchTerm) ||
-        pinball.publisher.toLowerCase().includes(lowerCaseSearchTerm)) &&
-      (this.publisherFilter ? pinball.publisher.toLowerCase().includes(this.publisherFilter.toLowerCase()) : true) &&
-      (this.genreFilter ? pinball.genre.toLowerCase().includes(this.genreFilter.toLowerCase()) : true)
-    );
-  
-    this.filteredConsoles = this.consoles.filter((console) =>
-      (console.name.toLowerCase().includes(lowerCaseSearchTerm) ||
-        console.genre.toLowerCase().includes(lowerCaseSearchTerm) ||
-        console.publisher.toLowerCase().includes(lowerCaseSearchTerm)) &&
-      (this.publisherFilter ? console.publisher.toLowerCase().includes(this.publisherFilter.toLowerCase()) : true) &&
-      (this.genreFilter ? console.genre.toLowerCase().includes(this.genreFilter.toLowerCase()) : true)
-    );
+
+    const filterFunction = (machine: Arcade) => 
+      (machine.name.toLowerCase().includes(lowerCaseSearchTerm) ||
+        (machine.genre && machine.genre.toLowerCase().includes(lowerCaseSearchTerm)) ||
+        machine.publisher.toLowerCase().includes(lowerCaseSearchTerm)) &&
+      (!this.publisherFilter || machine.publisher.toLowerCase().includes(this.publisherFilter.toLowerCase())) &&
+      (!this.genreFilter || (machine.genre && machine.genre.toLowerCase().includes(this.genreFilter.toLowerCase()))) &&
+      (!this.releaseFilter || (machine.release && machine.release.toString().includes(this.releaseFilter)));
+
+    this.filteredArcades = this.arcades.filter(filterFunction);
+    this.filteredPinballs = this.pinballMachines.filter(filterFunction);
+    this.filteredConsoles = this.consoles.filter(filterFunction);
   }
-  
+
+  updateUniqueFilters() {
+    this.uniquePublishers = [...new Set([...this.getUniquePublishers(this.arcades), ...this.getUniquePublishers(this.pinballMachines), ...this.getUniquePublishers(this.consoles)])];
+    this.uniqueGenres = [...new Set([...this.getUniqueGenres(this.arcades), ...this.getUniqueGenres(this.pinballMachines), ...this.getUniqueGenres(this.consoles)])];
+  }
+
   getUniquePublishers(data: Arcade[]): string[] {
-    return [...new Set(data.map(item => item.publisher))];
+    return [...new Set(data.map(item => item.publisher).filter(p => p))];
   }
+
   getUniqueGenres(data: Arcade[]): string[] {
-    return [...new Set(data.map(item => item.genre))];
+    return [...new Set(data.map(item => item.genre).filter(g => g))];
+  }
+
+  addMachineToUser(arcade: Arcade, machineType: string) {
+    this.userService.addMachineToUser(arcade, machineType).subscribe({
+      next: () => window.location.reload(),
+      error: (err: HttpErrorResponse) => alert(err.message)
+    });
   }
 }
