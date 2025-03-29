d6641a5ad336a8f1898d908b6bc57702b7855f3a
LeviiHackerman <barath.levente.2020@osztalyterem.katolikuskeri.hu>
2025-03-29 21:28:50 +0100

    Mostmár a profilban lévő adatokat csak a helyes formátumban lehet megadni

--- Frontend/project/src/app/userprofile/userprofile.component.ts
+++ Frontend/project/src/app/userprofile/userprofile.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { CompanyService } from '../services/companies.service';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-userprofile',
@@ -19,19 +19,79 @@ export class UserprofileComponent implements OnInit {
 
   constructor(private userService: UserService, private companyService: CompanyService) {  
     this.profilForm = new FormGroup({
-      name: new FormControl(''),
-      age: new FormControl(''),
-      phoneNumber: new FormControl(''),
-      profilePic: new FormControl(''),
+      name: new FormControl('', [Validators.required]),
+      age: new FormControl('', [Validators.required, Validators.min(12), Validators.max(116)]),
+      phoneNumber: new FormControl('', [Validators.required]),
+      profilePic: new FormControl('', [Validators.required]),
 
-      registrationNumber: new FormControl(''),
-      contactPerson: new FormControl(''),
-      websiteUrl: new FormControl(''),
+      registrationNumber: new FormControl('', [Validators.required]),
+      contactPerson: new FormControl('', [Validators.required]),
+      websiteUrl: new FormControl('', [Validators.required]),
     });
   }
 
   ngOnInit(): void {
     this.loadUserProfile();
+  }
+
+  onPhoneNumberInput(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    let value = input.value.replace(/\D/g, '');
+    
+    if (value.startsWith('06')) {
+      if (value.length > 2) {
+        value = value.slice(0, 2) + ' ' + value.slice(2);
+      }
+      if (value.length > 5) {
+        value = value.slice(0, 5) + ' ' + value.slice(5);
+      }
+      if (value.length > 9) {
+        value = value.slice(0, 9) + ' ' + value.slice(9);
+      }
+    }
+  
+    value = value.slice(0, 12); 
+  
+    input.value = value;
+    this.profilForm.get('phoneNumber')?.setValue(value);
+  }
+  
+  onRegistrationNumberInput(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    let value = input.value.replace(/\D/g, ''); 
+  
+    if (value.length > 2) {
+      value = value.slice(0, 2) + '-' + value.slice(2);
+    }
+    if (value.length > 5) {
+      value = value.slice(0, 5) + '-' + value.slice(5);
+    }
+    if (value.length > 11) {
+      value = value.slice(0, 11);
+    }
+  
+    input.value = value;
+    this.profilForm.get('registrationNumber')?.setValue(value);
+  }
+  
+  onWebsiteUrlInput(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    let value = input.value.trim();
+  
+    const urlPattern = /^(https?:\/\/)?([\w.-]+)\.([a-z]{2,})(\/\S*)?$/i;
+    if (value && !urlPattern.test(value)) {
+      input.classList.add('is-invalid'); 
+    } else {
+      input.classList.remove('is-invalid');
+    }
+  
+    this.profilForm.get('websiteUrl')?.setValue(value);
+  }
+  
+
+  isInvalid(fieldName: string): boolean {
+    const control = this.profilForm.get(fieldName);
+    return control ? control.invalid && control.touched : false;
   }
 
   loadUserProfile() {
